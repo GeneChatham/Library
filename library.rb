74532366f985423e26e2292458d25b4b5ac4f940
GeneChatham <genechatham@gmail.com>
2013-11-15 10:19:42 -0600

    added getter methods for book title and status, added list_books method

--- library.rb
+++ library.rb
@@ -12,7 +12,23 @@ class Library
   def books
   end
 
+  # Public: puts a list of books and their checkout status to the console
+  #
+  # Examples
+  #
+  #     listbooks
+  #     # => '"Jonathan Livingston Seagull" : available '
+  #     # => '"2001: A Space Odyysey" : checked out'
+  #
+  # Returns nothing 
   def list_books
+    @books.each do |book|
+      if book.checked_out
+        puts '"' + book.title + '" : checked out'
+      else
+        puts '"' + book.title + '" : avaiable'
+      end
+    end 
   end
 
   def borrowed_books
@@ -68,12 +84,38 @@ end
 # Public: The Book Class descibes a book with basic attributes
 class Book
 
-  # Public: Initialize a Book.
+  # Public: Initialize a Book that is not checked out.
   #
   # title - The String that will name the title of the Book
   # author - The String that will name the author of the Book
   def initialize(title, author)
     @title = title
     @author = author
+    @checked_out = false
   end
+
+  # Public: Returns the book title.
+  #
+  # Examples
+  #
+  #     2001.title
+  #     # => "2001: A Space Odyssey"
+  #
+  # Returns @title
+  def title
+    @title
+  end
+
+  # Public: Returns a Boolean indicating if the Book is checked out or not.
+  #
+  # Examples
+  #
+  #     2001.checked_out
+  #     # => true
+  #
+  # Returns @title
+  def checked_out
+    @checked_out
+  end
+
 end
